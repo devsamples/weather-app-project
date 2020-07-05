83c1d72bbf1f36389e8d00dbd14086e699ce8327
devsamples <kidotomas@gmail.com>
2020-07-05 18:31:45 +0200

    Updates

---
+++ precache-manifest.6f91efae0eca227ab307d8da664a59ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc18a9a2488d0e86f41f5a5c00e357f8",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "c7a5a94bd31fea5d4acb",
+    "url": "/weather-app/static/css/main.d826e37e.chunk.css"
+  },
+  {
+    "revision": "32eae9c168d92e60562a",
+    "url": "/weather-app/static/js/2.d32a2dca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.d32a2dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a5a94bd31fea5d4acb",
+    "url": "/weather-app/static/js/main.9c6fbb93.chunk.js"
+  },
+  {
+    "revision": "37c738ef312e86ba523e",
+    "url": "/weather-app/static/js/runtime-main.32279052.js"
+  }
+]);
